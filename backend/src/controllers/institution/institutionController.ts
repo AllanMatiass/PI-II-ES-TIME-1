2640ea1f4c6c3142c0fadff1cdb0fa2f0920820b
Allan Matias <allan.gmp@puccampinas.edu.br>
2025-10-19 14:15:09 -0300

    Develop to main (#69)
    * refactor: refatorando DB, seguindo modelo logico e normalizando + criando procedures uteis
    
    * fix: arrumando ordem de preocedencia no setup.sql
    
    * fix: arrumando ordem de preocedencia no setup.sql
    
    * refactor: refatorando db
    
    * refactor: refatorando db
    
    * fix: usuario esta sendo salvo em sessão
    
    * chore: fazendo setup inicial das instituicoes
    
    * feat: instituicao pode ser criada
    
    * feat: instituicao esta relacionada com professor
    
    * feat: instituicao esta relacionada com professor
    
    * feat: todos os professors/instituicoes pegos
    
    * feat: get instituicao por ID
    
    * feat: get instituicao por ID do professor
    
    * feat: relacionando professor com instituicao
    
    * refactor: Usando classe personalizada AppError para tratar erros mais facilmente
    
    * feat: ataulizar o nome de uma instituição implementado
    
    * feat: instituicao esta sendo deletada com sucesso
    
    * docs: crud de instituicao documentada (#66)
    
    Co-authored-by: Allan Giovanni Matias <alangiovanepaes@gmail.com>
    
    * docs: crud de instituicao documentada (#68)
    
    Co-authored-by: Allan Giovanni Matias <alangiovanepaes@gmail.com>
    
    ---------
    
    Co-authored-by: Allan Giovanni Matias <alangiovanepaes@gmail.com>
    Co-authored-by: Allan Giovanni Matias Paes <109719778+Matiaszz@users.noreply.github.com>

--- backend/src/controllers/institution/institutionController.ts
+++ backend/src/controllers/institution/institutionController.ts
@@ -1,25 +1,36 @@
+// Autor: Allan Giovanni Matias Paes
+// Controller de instituição
+
+// NOTA: todos os catch's estão padronizados para reconhecer o tipo AppError lançado em algum momento do código, caso não seja, é um erro inesperado.
 import { InstitutionRegisterRequestDTO, InstitutionResponseDTO, InstitutionWithProfessorsResponseDTO } from "dtos";
 import { Request, Response } from "express";
 import { insertInstitution, getAllInstitutions, getInstitutionById, getInstitutionByProfessorId, insertProfessorInInstitution, updateInstitution, deleteInstitution } from "../../services/institutionService";
 import { getLoggedUser } from "../../services/auth";
 import { AppError } from '../../errors/AppError';
 
+// cria uma instituição
 export async function createInstitution(req:Request, res: Response) {
     const {body} = req;
     try{
+        // verifica se tem campo "name" no body
         if (!body || !body.name){
             throw new AppError(400, "Property 'name' cannot be null.");
         }
 
+        // converte para um tipo customizado que contém apenas "name" como propriedade
         const data = body as InstitutionRegisterRequestDTO;
-  
+        
+        // Pega o professor logado e vê se está autenticado (a verificação foi feita apenas para evitar do Typescript reclamar.)
         const professor = getLoggedUser(req);
         if (!professor){
             throw new AppError(401, 'User is not authenticated.');
         }
+
+        // Faz a inserção via service
         const institution: InstitutionResponseDTO = await insertInstitution(data, professor.id);
 
         
+        // Retorna um JSON com a instituição criada
         return res.json({
             message: 'Institution was created successfully.',
             data: institution
@@ -36,6 +47,7 @@ export async function createInstitution(req:Request, res: Response) {
     
 }
 
+// Apenas pega todas as instituições e professores relacionados
 export async function findAllInstitutions(res: Response) {
     return res.json({
         message: 'All institutions are found.',
@@ -43,15 +55,19 @@ export async function findAllInstitutions(res: Response) {
     });
 }
 
+// Encontra instituição por ID
 export async function findInstitutionById(req: Request, res: Response) {
     try{
+        // Verifica se ID foi passado como parâmetro
         const {id} = req.params;
         if (!id){
             throw new AppError(400, 'Params must contain "id" field');
         }
 
+        // Faz o get via service.
         const institution = await getInstitutionById(id);
 
+        // retorna informações daquela instituição
         return res.json({
             message: 'Institution Found',
             data: institution
@@ -68,13 +84,16 @@ export async function findInstitutionById(req: Request, res: Response) {
     
 }
 
+// Encontra as instituições de um certo professor pelo ID.
 export async function findInstitutionByProfessorId(req: Request, res: Response) {
     try{
+        // Verifica se tem ID nos parâmetros
         const {id} = req.params;
         if (!id){
             throw new AppError(400, 'Params must contain "id" field');
         }
 
+        // Pega as instituições vinculadas à aquele professor
         const institution = await getInstitutionByProfessorId(id);
 
         return res.json({
@@ -93,8 +112,10 @@ export async function findInstitutionByProfessorId(req: Request, res: Response) 
     
 }
 
+// Rota de "adicionar professor a uma instituição"
 export async function relateProfessorWithInstitution(req: Request, res: Response) {
     try{
+            // Verifica se existe professorId e instituitionId no body
             const { body } = req;
 
             if (!body || body.professorId === undefined) {
@@ -106,9 +127,11 @@ export async function relateProfessorWithInstitution(req: Request, res: Response
             }
 
             const {professorId, institutionId} = body;
-    
+            
+            // Insere o professor naquela instituição via service
             const institution: InstitutionResponseDTO = await insertProfessorInInstitution(professorId, institutionId);
 
+            // Se tudo ocorrer bem, retorna aquela instituição recém vinculada
             return res.json({
                 message: 'Relationship between professor and institution was created successfully.',
                 data: institution
@@ -124,8 +147,11 @@ export async function relateProfessorWithInstitution(req: Request, res: Response
     }
 }
 
+
+// Atualiza uma instituição
 export async function putInstitution(req: Request, res: Response) {
     try{
+        // verifica se existe ID no params e um body na requisição.
         const {params, body} = req;
     
         if (!params.id){
@@ -136,6 +162,7 @@ export async function putInstitution(req: Request, res: Response) {
             throw new AppError(400, 'Body must contain data to continue.');
         }
 
+        // Atualiza a instituição e retorna a instituição atualizada.
         const updatedInstitution = await updateInstitution(params.id, body);
 
         return res.json({
@@ -154,14 +181,18 @@ export async function putInstitution(req: Request, res: Response) {
     
 }
 
+// Deleta uma instituição pelo ID
 export async function delInstitution(req: Request, res: Response) {
     try{
+        
+        // verifica se tem ID no parâmetro
         const {params} = req;
     
         if (!params.id){
             throw new AppError(404, 'Institution ID not found.');            
         }
 
+        // deleta uma instituição pelo ID e não retorna nada além de uma mensagem de sucesso
         await deleteInstitution(params.id);
 
         return res.json({
