7a6257d7521c39f376f8e05f390baacab2d97a11
Allan Matias <allan.gmp@puccampinas.edu.br>
2025-11-01 11:35:10 -0300

    Fix: ajustando .env (#93)
    * feat: rota de inserção de classe criada
    
    * feat: get classe por id
    
    * feat: get todas as classes
    
    * feat: get todas as classes de uma materia
    
    * feat: UPDATE de classe
    
    * fix: atualizando pastas
    
    * feat: delete class
    
    * fix: dotenv
    
    ---------
    
    Co-authored-by: Allan Giovanni Matias <alangiovanepaes@gmail.com>

--- backend/src/types/data-models.d.ts
+++ backend/src/types/data-models.d.ts
@@ -46,25 +46,6 @@ declare module 'dataModels' {
         end_date: Date;
     }
 
-    interface ClassDataModel {
-        id: string;
-        subject_id: string;
-        name: string;
-        classroom_location: string;
-        class_time: string;
-        class_date: Date;
-    }
-
-    interface SubjectDataModel {
-        id: string;
-        course_id: string;
-        name: string;
-        code: string;
-        acronym: string;
-        period: number;
-        start_date: Date;
-        end_date: Date;
-        professor_institution_id: string;
-    }
+ 
 }
 
