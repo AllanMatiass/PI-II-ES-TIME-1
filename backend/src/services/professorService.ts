ec31af894de7af13a6c204be1663b1592d2b76ad
Allan Matias <allan.gmp@puccampinas.edu.br>
2025-11-07 11:17:59 -0300

    feat: put professor

--- backend/src/services/professorService.ts
+++ backend/src/services/professorService.ts
@@ -1,5 +1,21 @@
 import { ProfessorDataModel } from "dataModels";
 import { DatabaseClient } from "../db/DBClient";
+import { ProfessorResponseDTO } from "dtos";
+import { AppError } from "../errors/AppError";
 
 const db = new DatabaseClient();
 const professorsTable = db.table<ProfessorDataModel>('professors');
+
+
+export async function updateProfessor(_id: string, update: Partial<ProfessorDataModel>) : Promise<ProfessorResponseDTO> {
+    const professorExist = await professorsTable.findUnique({id:_id});
+    if (!professorExist) throw new AppError(404, 'ID not found.');
+
+    const updateData = Object.fromEntries(
+        Object.entries(update).filter(([_, value]) => value !== undefined)
+    ) as Partial<ProfessorDataModel>;
+
+    await professorsTable.update(updateData, { id:_id });
+    const {id, name, email, phone, created_at} = await professorsTable.findUnique({ id:_id }) as ProfessorResponseDTO;
+    return {id, name, email, phone, created_at}
+}
