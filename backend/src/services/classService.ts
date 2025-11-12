ffc910930a8156799fa463d023c915e28fbae6d6
Cristian Eduardo Fava <93165583+DuckCoder1101@users.noreply.github.com>
2025-11-12 10:35:32 -0300

    BOMBA, Resolvendo erros da classe do SQL. (#140)

--- backend/src/services/classService.ts
+++ backend/src/services/classService.ts
@@ -1,114 +1,122 @@
 // Autor: Allan Giovanni Matias Paes
 
-import { ClassRegisterRequestDTO, ClassResponseDTO } from "dtos";
-import { AppError } from "../errors/AppError";
-import { DatabaseClient } from "../db/DBClient";
-import { ClassDataModel, CourseDataModel, InstitutionDataModel, SubjectDataModel } from "dataModels";
+import { ClassRegisterRequestDTO, ClassResponseDTO } from 'dtos';
+import { AppError } from '../errors/AppError';
+import { DatabaseClient } from '../db/DBClient';
+import {
+	ClassDataModel,
+	CourseDataModel,
+	InstitutionDataModel,
+	SubjectDataModel,
+} from 'dataModels';
 
 const db = new DatabaseClient();
-const institutionTable = db.table<InstitutionDataModel>('institutions');
 const subjectTable = db.table<SubjectDataModel>('subjects');
-const courseTable = db.table<CourseDataModel>('courses');
 const classTable = db.table<ClassDataModel>('classes');
 
+export async function insertClass(
+	data: ClassRegisterRequestDTO
+): Promise<ClassResponseDTO> {
+	const { subject_id, name, classroom } = data;
 
-export async function insertClass(data: ClassRegisterRequestDTO): Promise<ClassResponseDTO> {
-    const {subject_id, name, classroom_location, class_time, class_date} = data;
+	const subject = await subjectTable.findUnique({ id: subject_id });
 
-    const subject = await subjectTable.findUnique({id: subject_id});
-    
-    if (!subject){
-        throw new AppError(404, 'Subject not found.');
-    }
+	if (!subject) {
+		throw new AppError(404, 'Subject not found.');
+	}
 
-    const classId = await classTable.insert({subject_id, name, classroom_location, class_time, class_date});
-    const class_ = await classTable.findUnique({id: classId});
+	const classId = await classTable.insert({ subject_id, name, classroom });
+	const class_ = await classTable.findUnique({ id: classId });
 
-    if (!class_){
-        throw new AppError(404, 'Class not found.');
-    }
+	if (!class_) {
+		throw new AppError(404, 'Class not found.');
+	}
 
-    return {
-        id: classId,
-        subject_id,
-        name, 
-        classroom_location,
-        class_time,
-        class_date
-    }
+	return {
+		id: classId,
+		subject_id,
+		name,
+		classroom,
+	};
 }
 
 export async function findAllClasses(): Promise<ClassResponseDTO[]> {
-    const class_ = await classTable.findMany() || [];
+	const class_ = (await classTable.findMany()) || [];
 
-    return class_;
+	return class_;
 }
 
 export async function findClassByID(id: string): Promise<ClassResponseDTO> {
-    const class_ = await classTable.findUnique({id});
+	const class_ = await classTable.findUnique({ id });
 
-    if (!class_){
-        throw new AppError(404, 'Class not found.');
-    }
+	if (!class_) {
+		throw new AppError(404, 'Class not found.');
+	}
 
-    return class_;
+	return class_;
 }
 
-export async function findClassBySubjectId(subId: string): Promise<ClassResponseDTO[]> {
-    const subject = await subjectTable.findUnique({id: subId});
-    
-    if (!subject){
-        throw new AppError(404, 'Subject not found.');
-    }
+export async function findClassBySubjectId(
+	subId: string
+): Promise<ClassResponseDTO[]> {
+	const subject = await subjectTable.findUnique({ id: subId });
 
-    const classes = await classTable.findMany({
-        subject_id: subId
-    }) || [];
+	if (!subject) {
+		throw new AppError(404, 'Subject not found.');
+	}
 
-    return classes;   
+	const classes =
+		(await classTable.findMany({
+			subject_id: subId,
+		})) || [];
+
+	return classes;
 }
 
-export async function updateClass(id: string, data: Partial<ClassRegisterRequestDTO>): Promise<ClassResponseDTO> {
-    const classExist = await classTable.findUnique({ id });
-    if (!classExist) {
-        throw new AppError(404, 'Class not found.');
-    }
+export async function updateClass(
+	id: string,
+	data: Partial<ClassRegisterRequestDTO>
+): Promise<ClassResponseDTO> {
+	const classExist = await classTable.findUnique({ id });
+	if (!classExist) {
+		throw new AppError(404, 'Class not found.');
+	}
 
-    // Cria um objeto só com os campos que mudaram
-    const updateData: Partial<ClassRegisterRequestDTO> = {};
+	// Cria um objeto só com os campos que mudaram
+	const updateData: Partial<ClassRegisterRequestDTO> = {};
 
-    for (const key in data) {
-        if (Object.prototype.hasOwnProperty.call(data, key)) {
-            // @ts-ignore
-            if (classExist[key] && data[key] !== classExist[key]) {
-                // @ts-ignore
-                updateData[key] = data[key];
-            }
-        }
-    }
+	for (const key in data) {
+		if (Object.prototype.hasOwnProperty.call(data, key)) {
+			// @ts-ignore
+			if (classExist[key] && data[key] !== classExist[key]) {
+				// @ts-ignore
+				updateData[key] = data[key];
+			}
+		}
+	}
 
-    // Se não houver alterações, retorna a própria classe
-    if (Object.keys(updateData).length === 0) {
-        return classExist;
-    }
+	// Se não houver alterações, retorna a própria classe
+	if (Object.keys(updateData).length === 0) {
+		return classExist;
+	}
 
-    // Atualiza apenas os campos que mudaram
-    await classTable.update(data, {id});
-    const updatedClass = await classTable.findUnique({ id });
+	// Atualiza apenas os campos que mudaram
+	await classTable.update(data, { id });
+	const updatedClass = await classTable.findUnique({ id });
 
-    if (!updatedClass) {
-        throw new AppError(500, 'Something wrong happened');
-    }
-    
-    return updatedClass;
+	if (!updatedClass) {
+		throw new AppError(500, 'Something wrong happened');
+	}
+
+	return updatedClass;
 }
 
 export async function deleteClass(id: string): Promise<boolean> {
-    const classExist = await classTable.findUnique({ id });
-    if (!classExist) {
-        throw new AppError(404, 'Class not found.');
-    }
+	const classExist = await classTable.findUnique({ id });
+	if (!classExist) {
+		throw new AppError(404, 'Class not found.');
+	}
 
-    const res = await classTable.deleteMany({id});
-    return res;
+	const res = await classTable.deleteMany({ id });
+	return res;
 }
