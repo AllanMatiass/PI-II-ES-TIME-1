8f3c30dbd62239df82ae5f9c9d12a4168655b729
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-11-15 17:12:22 -0300

    feat: api de esqueci a senha consumida

--- backend/src/services/professorService.ts
+++ backend/src/services/professorService.ts
@@ -34,7 +34,7 @@ export async function updateProfessor(_id: string, update: Partial<ProfessorData
 }
 
 export async function generateRecoveryToken(resetTokens: Map<number, string>, email: string) {
-    const professor =await professorsTable.findUnique({email});
+    const professor = await professorsTable.findUnique({email});
 
 
     if (!professor){
@@ -59,6 +59,8 @@ export async function generateRecoveryToken(resetTokens: Map<number, string>, em
 	if (!emailStatus.success) {
         throw new AppError(500, 'Falha ao enviar e-mail.');
 	}
+
+    return token;
 }
 
 export async function recoverPassword(resetTokens: Map<number, string>,token: number, email: string, newPassword: string) {
